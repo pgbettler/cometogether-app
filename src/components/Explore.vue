5918878f344338b761d8b38ccaa3bd4cb5a9c81e
Pbettler <pbettler@gmail.com>
2019-06-29 10:47:41 -0400

    Explore & Login Changes
    Added Explore Page Content and Edited Login Component functionality - Justin

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,10 +1,58 @@
 <template>
+<<<<<<< .merge_file_qM8gaW
     <div>
         <p>Explore Page
         </p>
         <datetime type="date" v-model="date"></datetime>
+=======
+    <div id="explore">
+        <section>
+            <div class="col2">
+              <div v-if="posts.length">
+                  <div v-for="post in posts" class="post">
+                      <h5>{{ post.title }}</h5>
+                      <span>{{ post.createdOn | formatDate }}</span>
+                      <p>{{ post.content | trimLength }}</p>
+                      <ul>
+                          <li><a>likes {{ post.likes }}</a></li>
+                          <li><a>view full post</a></li>
+                      </ul>
+                  </div>
+              </div> 
+              <div v-else>
+                  <p class="no-results">There are currently no posts</p>
+              </div>
+            </div>
+        </section>
+>>>>>>> .merge_file_iXTV6D
     </div>
 </template>
-
 <script>
+<<<<<<< .merge_file_qM8gaW
+=======
+import { mapState } from "vuex";
+const fb = require("../../firebaseConfig.js");
+
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        content: '',
+        eventDate: '',
+        picture: ''
+      },
+      showEditForm: false,
+      editId: "",
+      search: "",
+      contactString: ""
+    };
+  },
+  computed: {
+    ...mapState(['userProfile', 'currentUser', 'posts']),
+  },
+  methods: {
+  }
+};
+>>>>>>> .merge_file_iXTV6D
 </script>
